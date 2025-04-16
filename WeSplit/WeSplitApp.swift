a746df067b089d52de16fe3b81989f27d0164339
Ramoju <sravan1.sriramoju@gmail.com>
2025-04-16 11:51:14 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sravan Sriramoju on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
